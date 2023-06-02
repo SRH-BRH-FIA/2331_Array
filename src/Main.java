e5fe0cc6b474156e118359bfca113ecb22645c85
Peter Dillinger <peter.dillinger@srh.de>
2023-06-02 11:37:05 +0200

    Vereinigungsmenge bilden: fasseZweiArraysZusammen

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,19 @@ public class Main {
         System.out.println("im Array an der Stelle " + indexDerKleinstenZahl);
     }
 
+    public static void findeDasMaximumUndGebeEsAus(double[] werte) {
+        double größteZahl = werte[0];
+        int indexDerGrößtenZahl = 0;
+        for (int i=1; i < werte.length; i++) {
+            if (werte[i] > größteZahl) {
+                größteZahl = werte[i];
+                indexDerGrößtenZahl = i;
+            }
+        }
+        System.out.println("Das Maximum ist " + größteZahl);
+        System.out.println("im Array an der Stelle " + indexDerGrößtenZahl);
+    }
+
     // Aufgabe: Mittelwert berechnen und an erster Stelle speichern
     public static void mittelwertBerechnen(double[] werte) {
         //werte.length: gibt die Größe des Arrays an
@@ -39,6 +52,19 @@ public class Main {
         double mittelwert = summe / werte.length;
         werte[0] = mittelwert;
     }
+
+    public static double[] fasseZweiArraysZusammen(double[] a, double[] b) {
+        int größeDesNeuenArrays = a.length + b.length;
+        double[] neuesArray = new double[ größeDesNeuenArrays ];
+        for (int i=0; i < a.length; i++) {
+            neuesArray[i] = a[i];
+        }
+        for (int i=0; i < b.length; i++) {
+            neuesArray[a.length + i] = b[i];
+        }
+        return neuesArray;
+    }
+
 
     public static void main(String[] args) {
 
@@ -70,9 +96,21 @@ public class Main {
         System.out.println("Der Mittelwert des Monatsarrays ist " +
                 monat[0]);
 
+        befülleArrayMitZufallszahlen( tag );
+
         mittelwertBerechnen( tag );
         System.out.println("Der Mittelwert des Tagarrays ist " +
                 tag[0]);
+
+        findeDasMinimumUndGebeEsAus(tag);
+        findeDasMaximumUndGebeEsAus(tag);
+
+        double[] a = { 3, 48, 99 };
+        double[] b = { 4, 32, 55, 78 };
+        double[] a_und_b = fasseZweiArraysZusammen(a, b);
+        for (int i=0; i<a_und_b.length; i++) {
+            System.out.print(a_und_b[i] + ", ");
+        }
     }
 
 }
